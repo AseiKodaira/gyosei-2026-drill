c2edb107e0752a7116377118a826b2fe5cdb5029
AseiKodaira <amor_usted@softbank.ne.jp>
2026-09-19 21:39:08 +0900

    Update offline service worker

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='gyosei2026-v2';
-const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon.svg','./data-init.js','./data-mock1.js','./data-mock2.js','./data-mock3.js','./data-predicted.js','./data-mnemonics.js','./data-prop-2016.js','./data-prop-2017.js','./data-prop-2018.js','./data-prop-2019.js','./data-prop-2020.js','./data-prop-2021.js','./data-prop-2022.js','./data-prop-2023.js','./data-prop-2024.js','./data-prop-2025.js'];
+const CACHE='gyosei2026-standalone-v3';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon.svg'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(resp=>{const copy=resp.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return resp;}).catch(()=>caches.match('./index.html'))));});
